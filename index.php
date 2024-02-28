4084aa0e8b826d844c706aac7a633fbbc352236e
gitmarkx <markarielmaata@gmail.com>
2024-02-28 10:21:07 +0800

    added the scss file structure

--- index.php
+++ index.php
@@ -3,9 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no" />
+
     <title>Front-end Template</title>
+    <meta name="description" content="front-end template" />
+
+    <!-- OGP META TAGS -->
+    <meta property="og:title" content="front-end template">
+    <meta property="og:description" content="front-end template">
+    <meta property="og:image" content="front-end template">
+    <meta property="og:url" content="front-end template">
+    <meta property="og:type" content="website">
+
+    <!-- CSS LINK -->
+    <link rel="stylesheet" href="/public/css/style.css">
 </head>
 <body>
-    
+    <header></header>
+    <main>
+        <section><p class="header01">12</p></section>
+    </main>
+    <footer></footer>
 </body>
 </html>
