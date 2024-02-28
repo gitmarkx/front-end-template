67ac586494e5d1bc83a1d6a078f25accaccd2bdc
gitmarkx <markarielmaata@gmail.com>
2024-02-28 16:48:41 +0800

    added sample page basic setup

---
+++ page-1/index.php
@@ -0,0 +1,37 @@
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/include/config.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title><?php echo SITE_NAME; ?> | Page 1</title>
+    <!-- Header meta -->
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/include/header_meta.php') ?>
+
+    <!-- CSS LINK -->
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/include/header_links.php') ?>   
+
+    <!-- Google Analytics -->
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/include/g-a.php') ?>
+
+</head>
+<body id="page1">
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/include/layout/content-tag.php') ?>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/include/layout/header.php') ?>
+
+    <main>
+        <section style="width: 100%;height:100vh;display:flex;align-items:center;justify-content:center;">
+            PAGE 1
+        </section>
+    </main>
+    
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/include/layout/footer.php') ?>
+
+    <!-- SCRIPTS LINK -->
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/include/footer_links.php') ?>   
+</body>
+</html>
